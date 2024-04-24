18d858562e1318d0f7f5d6dfe413300bb7593b5e
Tammy <tlpainter3@gmail.com>
2024-04-24 15:59:31 -0500

    added pokeball images

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import pokemon from "./pokemon";
 import PokeDex from './PokeDex';
 import './App.css'
-
+let imageName = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC4Y8z_gpDMY37eEXYA_Azm5go-MXmfA9ABA&s';
 const App = () => (
     <div>
-      <h1 className='App-header'>Pokedex</h1>
+      <h1 className='App-header'><img className='App-logo' src={imageName} alt="" /> Pokedex <img className='App-logo' src={imageName} alt="" /></h1>
       <PokeDex pokemon={pokemon}/>
     </div>
 )
